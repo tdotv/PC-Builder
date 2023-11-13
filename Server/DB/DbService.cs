83d1b45d38054b2f0e6e31b2dcc8172779b02ec4
Oleg Turelskiy <o.turelskiy@mail.ru>
2023-11-13 17:23:22 +0300

    refactoring DbService

--- Server/DB/DbService.cs
+++ Server/DB/DbService.cs
@@ -3,47 +3,44 @@ using System.Data.SqlClient;
 
 public class DbService : IDbService
 {
+    // private readonly IDbConnection? _db;
     private readonly string? _connectionString;
 
     public DbService(IConfiguration configuration)
     {
         _connectionString = configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("ConnectionString is null");
-        if (_connectionString != null) { using var connection = new SqlConnection(_connectionString); }   
+        // if (_connectionString != null) { _db = new SqlConnection(_connectionString); }   
     }
 
-    public async Task<T> GetAsync<T>(string command, object parms, SqlConnection connection)
+    private async Task<List<T>> ExecuteQueryAsync<T>(string command, object parms)
     {
-        await connection.OpenAsync();
-        T result;
-        result = (await connection.QueryAsync<T>(command, parms).ConfigureAwait(false)).FirstOrDefault() ?? throw new InvalidOperationException("Result is null");
-        return result;
+        using var connection = new SqlConnection(_connectionString);
+        await connection.OpenAsync().ConfigureAwait(false);
+        return (await connection.QueryAsync<T>(command, parms).ConfigureAwait(false)).ToList();
     }
 
-//    public async Task<List<T>> GetAll<T>(string command, object parms)
-//     {
-//         List<T> result = new List<T>();
-//         result = (await _db.QueryAsync<T>(command, parms)).ToList();
-//         return result;
-//     } 
+    public async Task<T> GetAsync<T>(string command, object parms)
+    {
+        return (await ExecuteQueryAsync<T>(command, parms)).FirstOrDefault()!;
+    }
 
-//     public async Task<T> Insert<T>(string command, object parms)
-//     {
-//         T result;
-//         result = _db.Query<T>(command, parms).FirstOrDefault();
-//         return result;
-//     }
+   public async Task<List<T>> GetAll<T>(string command, object parms)
+    {
+        return await ExecuteQueryAsync<T>(command, parms);
+    } 
+
+    public async Task<T> Insert<T>(string command, object parms)
+    {
+        return (await ExecuteQueryAsync<T>(command, parms)).FirstOrDefault()!;
+    }
  
-//     public async Task<T> Update<T>(string command, object parms)
-//     {
-//         T result;
-//         result = _db.Query<T>(command, parms).FirstOrDefault();
-//         return result;
-//     }
+    public async Task<T> Update<T>(string command, object parms)
+    {
+        return (await ExecuteQueryAsync<T>(command, parms)).FirstOrDefault()!;
+    }
 
-//     public async Task<T> Delete<T>(string command, object parms)
-//     {
-//         T result;
-//         result = _db.Query<T>(command, parms).FirstOrDefault();
-//         return result;
-//     }
+    public async Task<T> Delete<T>(string command, object parms)
+    {
+        return (await ExecuteQueryAsync<T>(command, parms)).FirstOrDefault()!;
+    }
 }
--- Shared/Interfaces/IDbService.cs
+++ Shared/Interfaces/IDbService.cs
@@ -4,9 +4,9 @@ using System.Data.SqlClient;
 
 public interface IDbService
 {
-  Task<T> GetAsync<T>(string command, object parms, SqlConnection connection);
-//   Task<List<T>> GetAll<T>(string command, object parms);
-//   Task<T> Insert<T>(string command, object parms);
-//   Task<T> Update<T>(string command, object parms);
-//   Task<T> Delete<T>(string command, object parms);
+  Task<T> GetAsync<T>(string command, object parms);
+  Task<List<T>> GetAll<T>(string command, object parms);
+  Task<T> Insert<T>(string command, object parms);
+  Task<T> Update<T>(string command, object parms);
+  Task<T> Delete<T>(string command, object parms);
 }
